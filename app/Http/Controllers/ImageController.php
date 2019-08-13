d79e8994f761dc974ae52ecb65df3b6110ff5c6f
Sebastian Currea <sebastian.currea@gmail.com>
2019-08-13 14:35:14 +1000

    Test Imagick

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
+use Imagick;
+
 
 class ImageController extends Controller
 {
     public function convertPdf(Request $request){
-        $file =  $request['upload_file'];
-        dd($file);
+        try {
+            $fileName =  $request['upload_file']->getClientOriginalName();
+            $public_path = app()->make('path.public').(DIRECTORY_SEPARATOR. "pdf");
+            $request['upload_file']->move($public_path, $fileName);
+            $path = $public_path.DIRECTORY_SEPARATOR;
+            $imagick = new Imagick($path.$fileName);
+            dd($imagick);
+            $imagick->setResolution(300,300);
+            $imagick->writeFile($path.'pageone.jpg');
+
+            //$result = $imagick->readImage($path);
+            //return $imagick;
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
+        }
     }
+
+    public function testImage(){
+        $imagick = new Imagick();
+        $imagick->setResolution(300,300);
+        //return $imagick;
+        $result = $imagick->readImage("C:\Users\sc9614\Pictures\Camera Roll\document_20190805.pdf");
+        return $result;
+    }
+
 }
